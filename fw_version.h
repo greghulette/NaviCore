328047529607d0ddb3d4d1f7fbec3d2dff653f94
greghulette <31554549+greghulette@users.noreply.github.com>
2026-08-21 15:12:12 -0400

    Update index.html

--- fw_version.h
+++ fw_version.h
@@ -26,5 +26,5 @@
 #pragma once
 
 #define FW_VERSION_BASE  "v0.2.0"
-#define FW_VERSION_DTG   "200959QAUG26"
+#define FW_VERSION_DTG   "211512QAUG26"
 #define FW_VERSION       FW_VERSION_BASE "_" FW_VERSION_DTG
